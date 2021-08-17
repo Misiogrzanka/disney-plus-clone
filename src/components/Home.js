ab009293490fdefa34807fd8587137888e2e46e3
Misiogrzanka <77200161+Misiogrzanka@users.noreply.github.com>
2021-08-17 13:45:22 +0200

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function Home() {
-	return <div>Test</div>;
+	return <div>Home</div>;
 }
 
 export default Home;
+
+const Container = styled.main``;
