82436041a93704f9fc84fc8f8f4637e6a2996340
Misiogrzanka <77200161+Misiogrzanka@users.noreply.github.com>
2021-08-22 03:00:29 +0200

    Installed react-router-dom ADD: Detail controls template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
+import Detail from "./components/Detail";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="app">
-			<Header />
-			<Home />
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route path="/detail">
+						<Detail />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
